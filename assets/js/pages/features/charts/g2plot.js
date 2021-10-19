7a71f4230d16a0e682cee349a097ec45df95951f
Mariano Reyes <60166093+MarianoReyes@users.noreply.github.com>
2021-10-19 14:50:54 -0600

    cambio de graficas exitoso

--- assets/js/pages/features/charts/g2plot.js
+++ assets/js/pages/features/charts/g2plot.js
@@ -19,100 +19,159 @@ var form = parseFloat(form1.innerHTML);
 var leg = parseFloat(leg1.innerHTML);
 var gob = parseFloat(gob1.innerHTML);
 
-
-import { Column } from '@antv/g2plot';
+console.log(acc);
 
 const data = [
-  {
-    name: 'London',
-    月份: 'Jan.',
-    月均降雨量: 18.9,
-  },
-  {
-    name: 'London',
-    月份: 'Feb.',
-    月均降雨量: 28.8,
-  },
-  {
-    name: 'London',
-    月份: 'Mar.',
-    月均降雨量: 39.3,
-  },
-  {
-    name: 'London',
-    月份: 'Apr.',
-    月均降雨量: 81.4,
-  },
-  {
-    name: 'London',
-    月份: 'May',
-    月均降雨量: 47,
-  },
-  {
-    name: 'London',
-    月份: 'Jun.',
-    月均降雨量: 20.3,
-  },
-  {
-    name: 'London',
-    月份: 'Jul.',
-    月均降雨量: 24,
-  },
-  {
-    name: 'London',
-    月份: 'Aug.',
-    月均降雨量: 35.6,
-  },
-  {
-    name: 'Berlin',
-    月份: 'Jan.',
-    月均降雨量: 12.4,
-  },
-  {
-    name: 'Berlin',
-    月份: 'Feb.',
-    月均降雨量: 23.2,
-  },
-  {
-    name: 'Berlin',
-    月份: 'Mar.',
-    月均降雨量: 34.5,
-  },
-  {
-    name: 'Berlin',
-    月份: 'Apr.',
-    月均降雨量: 99.7,
-  },
-  {
-    name: 'Berlin',
-    月份: 'May',
-    月均降雨量: 52.6,
-  },
-  {
-    name: 'Berlin',
-    月份: 'Jun.',
-    月均降雨量: 35.5,
-  },
-  {
-    name: 'Berlin',
-    月份: 'Jul.',
-    月均降雨量: 37.4,
-  },
-  {
-    name: 'Berlin',
-    月份: 'Aug.',
-    月均降雨量: 42.4,
-  },
+    {
+      "Categoría": "Prod de Datos",
+      "Puntaje": recd,
+      "type": "Puntaje"
+    },
+    {
+      "Categoría": "Disponibilidad",
+      "Puntaje": disp,
+      "type": "Puntaje"
+    },
+    {
+      "Categoría": "Plataformas",
+      "Puntaje": plat,
+      "type": "Puntaje"
+    },
+    {
+      "Categoría": "Acceso",
+      "Puntaje": acc,
+      "type": "Puntaje"
+    },
+    {
+      "Categoría": "Actualización",
+      "Puntaje": per,
+      "type": "Puntaje"
+    },
+    {
+      "Categoría": "Licencia",
+      "Puntaje": lic,
+      "type": "Puntaje"
+    },
+    {
+      "Categoría": "Formatos",
+      "Puntaje": form,
+      "type": "Puntaje"
+    },
+    {
+      "Categoría": "Legibilidad",
+      "Puntaje": leg,
+      "type": "Puntaje"
+    },
+    {
+      "Categoría": "Gobernanza",
+      "Puntaje": gob,
+      "type": "Puntaje"
+    },{
+      "Categoría": "Prod de Datos",
+      "Puntaje": recd,
+      "type": "Puntaje"
+    },
+    {
+      "Categoría": "Disponibilidad",
+      "Puntaje": disp,
+      "type": "Puntaje"
+    },
+    {
+      "Categoría": "Plataformas",
+      "Puntaje": plat,
+      "type": "Puntaje"
+    },
+    {
+      "Categoría": "Acceso",
+      "Puntaje": acc,
+      "type": "Puntaje"
+    },
+    {
+      "Categoría": "Actualización",
+      "Puntaje": per,
+      "type": "Puntaje"
+    },
+    {
+      "Categoría": "Licencia",
+      "Puntaje": lic,
+      "type": "Puntaje"
+    },
+    {
+      "Categoría": "Formatos",
+      "Puntaje": form,
+      "type": "Puntaje"
+    },
+    {
+      "Categoría": "Legibilidad",
+      "Puntaje": leg,
+      "type": "Puntaje"
+    },
+    {
+      "Categoría": "Gobernanza",
+      "Puntaje": gob,
+      "type": "Puntaje"
+    },
+    //Aqui el puntaje esperado
+    /*
+    {
+        "Categoría": "Prod de Datos",
+        "Puntaje": 8,
+        "type": "Puntaje Esperado"
+      },
+      {
+        "Categoría": "Disponibilidad",
+        "Puntaje": 8,
+        "type": "Puntaje Esperado"
+      },
+      {
+        "Categoría": "Plataformas",
+        "Puntaje": 8,
+        "type": "Puntaje Esperado"
+      },
+      {
+        "Categoría": "Acceso",
+        "Puntaje": 8,
+        "type": "Puntaje Esperado"
+      },
+      {
+        "Categoría": "Actualización",
+        "Puntaje": 8,
+        "type": "Puntaje Esperado"
+      },
+      {
+        "Categoría": "Licencia",
+        "Puntaje": 8,
+        "type": "Puntaje Esperado"
+      },
+      {
+        "Categoría": "Formatos",
+        "Puntaje": 8,
+        "type": "Puntaje Esperado"
+      },
+      {
+        "Categoría": "Legibilidad",
+        "Puntaje": 8,
+        "type": "Puntaje Esperado"
+      },
+      {
+        "Categoría": "Gobernanza",
+        "Puntaje": 8,
+        "type": "Puntaje Esperado"
+      }*/
 ];
+
+
+const { Column } = G2Plot;
+
 
 const stackedColumnPlot = new Column('container2', {
   data,
   isGroup: true,
-  xField: 'Categorías',
+  xField: 'Categoría',
   yField: 'Puntaje',
-  seriesField: 'name',
+  seriesField: 'type',
   /** 设置颜色 */
-  //color: ['#1ca9e6', '#f88c24'],
+  color: ['#1aac8a', '#40c7a7'],
   /** 设置间距 */
   // marginRatio: 0.1,
   label: {
@@ -131,3 +190,5 @@ const stackedColumnPlot = new Column('container2', {
 });
 
 stackedColumnPlot.render();
+
+
