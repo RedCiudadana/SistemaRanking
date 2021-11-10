d3462dc657e4c32c6d7453a0de9ac40512c32e70
Mariano Reyes <60166093+MarianoReyes@users.noreply.github.com>
2021-11-10 16:45:42 -0600

    nuevos links

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -2,7 +2,6 @@ const button = document.querySelector('button');
 const popup = document.querySelector('.popup-wrapper');
 const close = document.querySelector('.popup-close');
 
-console.log("hola");
 
 button.addEventListener('click', () => {
     popup.style.display = 'block';
