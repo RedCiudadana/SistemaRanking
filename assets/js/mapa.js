49fa0860ff4031f07dffdee50dc4a5dc7423c7ee
Mariano Reyes <60166093+MarianoReyes@users.noreply.github.com>
2021-11-29 15:52:04 -0600

    cambios a mapa, dejando fondo blanco

--- assets/js/mapa.js
+++ assets/js/mapa.js
@@ -65,14 +65,57 @@ function zoomToFeature(e) {
 
 // Code goes here
 
-var map = L.map('map').setView([14.6229, -90.5315], 8)
+L.Mask = L.Polygon.extend({
+	options: {
+		stroke: false,
+		color: '#fff',
+		fillOpacity:1,
+		clickable: true,
 
-L.tileLayer(
-  "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-  {
-    maxZoom: 18,
-  }
-).addTo(map);
+		outerBounds: new L.LatLngBounds([-90, -360], [90, 360])
+	},
+
+	initialize: function (latLngs, options) {
+        
+         var outerBoundsLatLngs = [
+			this.options.outerBounds.getSouthWest(),
+			this.options.outerBounds.getNorthWest(),
+			this.options.outerBounds.getNorthEast(),
+			this.options.outerBounds.getSouthEast()
+		];
+        L.Polygon.prototype.initialize.call(this, [outerBoundsLatLngs, latLngs], options);	
+	},
+
+});
+L.mask = function (latLngs, options) {
+	return new L.Mask(latLngs, options);
+};
+
+var lat = 15.75277501902558;
+var lng =  -90.368;
+var zoom =  8;
+
+
+var map = L.map('map').setView([lat, lng], zoom)
+
+map.options.minZoom = 7;
+map.options.maxZoom = 11;
+
+var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+var osmAttrib='Map data &copy; OpenStreetMap contributors';
+var osm = new L.TileLayer(osmUrl, {minZoom: 3, maxZoom: 8, attribution: osmAttrib});
+map.addLayer(osm);
+
+map.setView(new L.LatLng(lat, lng), zoom);
+
+// transform geojson coordinates into an array of L.LatLng
+var coordinates = guatemala.features[0].geometry.coordinates[0];
+var latLngs = [];
+for (i=0; i<coordinates.length; i++) {
+    latLngs.push(new L.LatLng(coordinates[i][1], coordinates[i][0]));
+}
+
+L.mask(latLngs).addTo(map);
 
 var mundata = L.geoJson(mundata, {
   style: style,
