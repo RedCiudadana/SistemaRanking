eb6b398babd6373a4af0c181078bf187007d561d
José Mariano Reyes <josemarianoreyesh02@gmail.com>
2021-12-21 15:12:53 -0600

    hitos

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,19 @@
 const compromisos = [
     "Gobierno Digital y Modernización de la Gestión Pública",
-    "Migracion",
+    "Educación, Salud, Seguridad Alimentaria y Nutricional",
+    "Medio Ambiente, Cambio Climático y Gestión Integral del Riesgo de Desastres",
+    "Reducción de la Pobreza y Migración",
+    "Inclusión Social y Poblaciones en Condiciones de Vulnerabilidad",
+    "Gestión y seguimiento de Iniciativas, Tratados y Convenios Internacionales en materia de Transparencia, Anticorrupción y Rendición de Cuentas"
+];
+
+const idcompromisos = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6"
 ];
 
 module.exports = function (eleventyConfig) {
@@ -22,6 +35,18 @@ module.exports = function (eleventyConfig) {
 
             let collection = collectionApi.getFilteredByTags('hitos').filter(function (item) {
                 return item.data.hitos.Compromiso === compromiso;
+            });
+
+            return collection;
+
+        });
+    });
+
+    idcompromisos.forEach((idcomp) => {
+        eleventyConfig.addCollection(idcomp, function (collectionApi) {
+
+            let collection = collectionApi.getFilteredByTags('hitos').filter(function (item) {
+                return item.data.hitos.idCompromiso === idcomp;
             });
 
             return collection;
